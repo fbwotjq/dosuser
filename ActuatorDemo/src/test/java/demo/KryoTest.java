1ce34968d5a4927751555f59089e355860965f37
dosuser <dosuser@naver.com>
2015-03-28 21:51:19 +0900

    test for kryo

--- ActuatorDemo/src/test/java/demo/KryoTest.java
+++ ActuatorDemo/src/test/java/demo/KryoTest.java
@@ -4,6 +4,13 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import org.apache.http.util.ByteArrayBuffer;
+import org.apache.tomcat.util.http.fileupload.ByteArrayOutputStream;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.util.StopWatch;
+
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
@@ -17,12 +24,42 @@ public class KryoTest {
 		
 	}*/
 	
-	public static void main (String[] args) throws IOException { 
-		Log.TRACE(); 
-		Kryo kryo = new Kryo(); 
-		//Set serializer that can handle added & removed fields (but can't 
-		// handle type change). 
+	Kryo kryo = null;
+	@Before
+	public void initDefaultKyro(){
+		kryo = new Kryo();
 		kryo.setDefaultSerializer(CompatibleFieldSerializer.class); 
+		kryo.register(TestKryoData.class);
+	}
+	
+	private final int loop = 10000;
+	/**
+	 * tuning is evil.
+	 *                                    create     ser   deser   total   size  +dfl
+	 * kryo                                   63     655     838    1493    212   132
+	 * kryo-manual                            63     555     616    1171    211   131
+	 */
+	@Test
+	public void serializeTimingTest(){
+		
+		Output output = new Output( new ByteArrayOutputStream(10000));
+		for(int l=0;l<loop;l++){
+			TestKryoData serializableInstance = new TestKryoData();
+		}
+		TestKryoData serializableInstance = new TestKryoData();
+		
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
+		for(int l=0;l<loop;l++){
+			kryo.writeObject(output, serializableInstance);
+		}
+		stopWatch.stop();
+		System.out.println(stopWatch.prettyPrint());
+	}
+	
+	@Test
+	public void legacyTest() throws IOException { 
+		Log.TRACE(); 
 		if (false) { 
 			Output output = new Output(new FileOutputStream("kryo.dat")); 
 			kryo.writeObject(output, new TestKryoData());
